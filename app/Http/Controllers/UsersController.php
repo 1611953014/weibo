9603a427d917fe9e08eede2b23bfc4e96e6c16ba
高槻泉 <1611953014@qq.com>
2020-05-17 22:10:09 +0800

    用户显示页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function create()
+    {
+        return view('users.create');
+    }
+
+    //TODO NPM yarn未安装
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+}
